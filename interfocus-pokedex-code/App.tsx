fe397cb7c08cd3d1177c4a60733d4bc84fecad4b
Mauro Maciel <mauromaciel1@outlook.com>
2022-05-10 17:11:32 -0300

    exportando tema padrao

--- interfocus-pokedex-code/App.tsx
+++ interfocus-pokedex-code/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
+import { ThemeProvider } from 'styled-components';
 import { Container, Texto } from './styles';
+import theme from './src/styles/theme'
 
 export default function App() {
   return (
-    <Container>
-      <Texto>
-        Open up App.js to start working on your app!
-      </Texto>
-    </Container>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Texto>
+          Open up App.js to start working on your app!
+        </Texto>
+      </Container>
+    </ThemeProvider>
   );
 }
